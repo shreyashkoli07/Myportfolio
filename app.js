bda5894d76f41bb9eabb8c7a329f2ea782903920
shreyashkoli07 <shreyashkoli4141@gmail.com>
2024-09-02 16:32:49 +0530

    first

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const routes = require('./routers/FormRoutes');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 //username 
 // shreyashkoli4141
